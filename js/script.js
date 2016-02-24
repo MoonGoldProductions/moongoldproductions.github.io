cf07fc37fae6f01d3106caa85c48559d74fdbaa6
Joonas Palm <joonas.palm.001@gmail.com>
2016-02-24 16:58:32 +0100

    First code upload. Basic structure.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var app = angular.module('mgpApp', []);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/kodu', {
+      templateUrl: 'view-kodu.html',
+      controller: 'koduCtrl'
+    }).
+    when('/teenused', {
+      templateUrl: 'view-teenused.html'
+    }).
+    when('/projektid', {
+      templateUrl: 'view-projektid.html'
+    }).
+    otherwise({
+      redirectTo: '/kodu',
+      controller: 'koduCtrl'
+    });
+  }
+]);
+
+app.controller('koduCtrl',
+  function($scope) {
+    
+  }
+);
